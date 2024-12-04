cf03a48b5dd27ec6baa4880c8bb928c58323c67b
Simon Phillips <simon@icuk.net>
2024-12-04 13:51:25 +0000

    Day 4 Start

---
+++ 2024/Day04/Day04.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode._2024.Day04 {
+    internal class Day04 : ISolution {
+        public string SolvePartOne(string input) {
+            string[] lines = AdventHelper.LineSplitInput(input);
+
+            throw new NotImplementedException();
+        }
+
+        public string SolvePartTwo(string input) {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
